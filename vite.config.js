8ca2218574b99de5a87dbb41a8b7e065b927abfe
Ujjwal Gupta <ujjwal.gupta@aula.ac.uk>
2025-07-08 19:48:03 +0530

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // This is the base path for the project.
   // It is used to serve the project from a subdirectory.
-  // For example, if the project is hosted at https://yourusername.github.io/vanilla-js,
-  // then the base path should be 'vanilla-js'.
+  // For example, if the project is hosted at https://yourusername.github.io/vaniila-js,
+  // then the base path should be 'vaniila-js'.
   // This is useful for deploying the project to GitHub Pages.
-  base: 'vanilla-js',
+  base: '/vaniila-js/',
   build: {
     outDir: 'docs',
   },
